563fc857e3f2b4a11d3cec75c4cbc69c5cc8f89e
Joshua Douty <Joshua.Douty@gmail.com>
2016-05-02 09:31:30 -0400

    Still using the meeting-cost hack. Add the total cost with a 30% addition for opportunity cost

--- app/components/meeting-cost.js
+++ app/components/meeting-cost.js
@@ -20,5 +20,22 @@ export default Ember.Component.extend({
 
 		var formatted = parseFloat(runningCost, 10).toFixed(2);
 		return '$' + formatted;
+	}),
+	totalWithOpportunityCost : Ember.computed('participants.@each.numOf', 'getMeetingLength.content.time', function() {
+		var runningCost = 0;
+
+		for (var i = 0; i < this.get('participants.length'); i++) {
+			var currentTime = this.get('store').peekAll('time').objectAt(0).get('time');
+			//var currentTime = this.get('participants').objectAt(i).get('time');
+
+			var participant = this.get('participants').objectAt(i);
+			var numberOf = participant.get('numOf');
+			var perMin = participant.get('perMinute')
+			runningCost = (runningCost + ((numberOf * perMin) * currentTime));
+		}
+
+		runningCost = runningCost * 1.3;
+		var formatted = parseFloat(runningCost, 10).toFixed(2);
+		return '$' + formatted;
 	})
 });
